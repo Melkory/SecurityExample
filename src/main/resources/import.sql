9c8d0ea4c51ebafe230b3f26a4a20ff90e156d27
Melkory <carloseduardo.dziombra@hotmail.com>
2024-11-29 19:29:17 -0300

    BCrypt password

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$4/ClppA5n4xnvLz6kpqNBOZlr3oh1nMxcbWwEx3Uq7R57QKpaWDdC');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$4/ClppA5n4xnvLz6kpqNBOZlr3oh1nMxcbWwEx3Uq7R57QKpaWDdC');
 
 INSERT INTO tb_product (name) VALUES ('TV');
 INSERT INTO tb_product (name) VALUES ('Computer');
